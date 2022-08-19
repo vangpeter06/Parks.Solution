11bd13b1bd5467cd7cc8dfcf2ff8bd54e3e648e3
Peter Vang <vang_peter06@yahoo.com>
2022-08-19 15:31:44 -0700

    add validations

--- Parks/Models/Park.cs
+++ Parks/Models/Park.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Parks.Models
 {
-  public class Park
-  {
-    public int ParkId { get; set; }
-    public string Name { get; set; }
-    public string Location { get; set; }
-    public string Type { get; set; }
-  }
-}    
+    public class Park
+    {
+        public int ParkId { get; set; }
+
+        [Required]
+        [StringLength(85)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(14)]
+        public string Location { get; set; }
+
+        [Required]
+        [StringLength(8)]
+        public string Type { get; set; }
+    }
+}
