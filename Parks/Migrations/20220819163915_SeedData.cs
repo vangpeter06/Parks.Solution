b85f74dbfe077c7e8ab6b492e345fe2b744eca84
Peter Vang <vang_peter06@yahoo.com>
2022-08-19 09:40:53 -0700

    add seed data migration

---
+++ Parks/Migrations/20220819163915_SeedData.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parks.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Location", "Name", "Type" },
+                values: new object[,]
+                {
+                    { 1, "Oregon", "Crater Lake", "National" },
+                    { 2, "California", "Yosemite National Park", "National" },
+                    { 3, "Utah", "Bryce Canyon Nation Park", "National" },
+                    { 4, "Utah", "Zion National Park", "National" },
+                    { 5, "Oregon", "Blue Lake", "State" },
+                    { 6, "California", "Merced's Applegate Park Zoo", "State" },
+                    { 7, "Oregon", "Fairview Community Park", "City" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 7);
+        }
+    }
+}
